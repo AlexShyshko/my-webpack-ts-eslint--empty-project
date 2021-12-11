8ab12f6193e22b0cde11d038b42f7bdbecd63d11
AlexShyshko <shish1988@mail.ru>
2021-12-12 00:10:18 +0300

    webpack-ts-eslint--empty-project

---
+++ src/script.ts
@@ -0,0 +1,14 @@
+import './css/normalize.css';
+import './css/style.css';
+//import './ts/';
+// const img = require('./assets/img/bg.jpg');
+// document.body.innerHTML = `<img src=${img} alt= />`;
+// const img: HTMLImageElement = new Image();
+// img.src = `./assets/img/1.png`;
+// document.body.insertAdjacentElement('beforeend', img);
+// document.body.appendChild(img);
+
+// console.log(
+// eslint-disable-next-line max-len
+//   'npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser clean-webpack-plugin copy-webpack-plugin css-loader eslint eslint-config-airbnb-typescript eslint-plugin-import eslint-webpack-plugin html-webpack-plugin mini-css-extract-plugin sass sass-loader ts-loader typescript webpack webpack-cli webpack-dev-server'
+// );
